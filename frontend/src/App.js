5b1e5ac74a8a09ceee22aed156b93d16bdb81c5b
Naveen <naveenn7271@gmail.com>
2024-08-28 22:40:39 +0530

    updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -33,8 +33,8 @@ const App = () => {
   };
 
   return (
-    <div className="min-h-screen bg-white flex flex-col overflow-y-scroll no-scrollbar">
-      <header className="bg-black rounded-t-2xl shadow">
+<div className="scroll-container min-h-screen bg-white flex flex-col ">
+<header className="bg-black rounded-t-2xl shadow">
         <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
           <h1 className="text-2xl text-white">Abstract | Help Center</h1>
           <button
